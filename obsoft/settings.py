08126aee897093b00705d5719b8ddabaf4939270
Vikaskoppoju <98159254+Vikaskoppoju@users.noreply.github.com>
2023-05-03 15:23:04 +0530

    Update settings.py

--- obsoft/settings.py
+++ obsoft/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&i8gd)jglbb)p&g27o(0ucoao-f0@rlou$d$t47y+lrsq--lsz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
